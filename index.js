519b8db6449f642e051b956204635d83c09a9e8b
dominictoms <dominictoms97@gmail.com>
2020-11-24 09:20:14 +0000

    stores output to json

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended: false});
+var fs = require("fs");
 
 app.use(express.static("public"));
 
 app.get("/index.html", function(req, res)
 {
-    res.sendFile(_dirname + "/" + "index.html");
+	res.sendFile(_dirname + "/" + "index.html");
 })
 
 app.post("/getstring", urlencodedParser, function(req, res)
 {
 	// generate response object
-    response = 
+	response = 
 	{
 		Forename: req.body.Forename,
 		Surname: req.body.Surname,
@@ -30,12 +31,23 @@ app.post("/getstring", urlencodedParser, function(req, res)
 		TestSlot: req.body.TestSlot
 	};
 
-    console.log(response);
-    res.end(JSON.stringify(response));
+	// output the response object to the console
+	console.log(response);
+
+	fs.appendFile("results.json", JSON.stringify(response), function (err)
+	{
+		if (err) throw err;
+		console.log('The "data to append" was appended to file!');
+	});
+
+	// tell the user if the form signup was succesful or unsuccesful
+	//res.status(404).send("signup failed (404)");
+	//res.status(500).send("signup failed (500)");
+	res.end("signup succesful!");
 })
 
 app.listen(3000, function()
 {
-	console.log("listening");
+	console.log("server is online and listening!");
 });
 
